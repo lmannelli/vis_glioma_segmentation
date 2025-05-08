90e5fac90b600c548f078f130fc3c599a208fedb
Lucciano Mannelli <lmannelli@entelai.com>
2025-05-08 16:20:50 -0300

    new degugging

--- train.py
+++ train.py
@@ -123,7 +123,8 @@ def train_epoch(model, loader, optimizer, loss_fn, scaler, device):
 
         # 2) Tiempo de copia a GPU
         t_to_start = time.time()
-        imgs = imgs_raw.to(device, non_blocking=True).contiguous(memory_format=torch.channels_last)
+        imgs = imgs_raw.to(device, non_blocking=True) \
+             .contiguous(memory_format=torch.channels_last_3d)
         lbls = lbls_raw.to(device, non_blocking=True)
         t_to = time.time() - t_to_start
 
@@ -164,7 +165,8 @@ def validate(model, loader, inferer, post_sigmoid, post_pred, acc_fn, device):
     model.eval()
     all_preds, all_lbls = [], []
     for batch in loader:
-        imgs = batch["image"].to(device, non_blocking=True).contiguous(memory_format=torch.channels_last)
+        imgs = batch["image"].to(device, non_blocking=True) \
+             .contiguous(memory_format=torch.channels_last_3d)
         lbls = decollate_batch(batch["label"].to(device))
         logits = inferer(imgs)
         all_preds.extend(decollate_batch(logits))
